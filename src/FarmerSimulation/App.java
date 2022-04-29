7ce1409a8be930d81d79bd9941534dea7330c023
CWH <ahaoahao2000@gmail.com>
2022-04-29 13:59:18 +0800

    Launch File Updated - Wei Hao

--- src/FarmerSimulation/App.java
+++ src/FarmerSimulation/App.java
@@ -3,9 +3,13 @@ package FarmerSimulation;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, 2 World!");
-        System.out.println("Hello, 2 World!");
-        System.out.println("Hello, 2 World!");
-        System.out.println("Hello, 2 World!");
-        System.out.println("Hello, 2 World!");
+        System.out.println("Hello, 3 World!");
+        System.out.println("Hello, 4 World!");
+        System.out.println("Hello, 5 World!");
+        System.out.println("Hello, 6 World!");
+        System.out.println("Hello, 7 World!");
+        System.out.println("Hello, 8 World!");
+        System.out.println("peepoPogClimbingTreeHard4House");
+        System.out.println("Woohoo weihao bring fly");
     }
 }
