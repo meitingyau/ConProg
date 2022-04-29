626f61b8c06bf958affac545a069063abc0a8235
CWH <ahaoahao2000@gmail.com>
2022-04-29 13:18:16 +0800

    [Wei Hao Testing]

--- src/FarmerSimulation/App.java
+++ src/FarmerSimulation/App.java
@@ -3,5 +3,9 @@ package FarmerSimulation;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, 2 World!");
+        System.out.println("Hello, 2 World!");
+        System.out.println("Hello, 2 World!");
+        System.out.println("Hello, 2 World!");
+        System.out.println("Hello, 2 World!");
     }
 }
