e11b06859dc068ebf28bb13fa734dff9b2cd67b7
CWH <ahaoahao2000@gmail.com>
2022-05-27 17:31:18 +0800

    Testing Git Ignore

--- src/FarmerSimulation/Activity.java
+++ src/FarmerSimulation/Activity.java
@@ -1,5 +1,8 @@
 package FarmerSimulation;
 
+import java.io.*;
+import java.sql.*;
+
 public class Activity {
     private String activityId;
     private String userId;
@@ -24,6 +27,30 @@ public class Activity {
         this.quantity = quantity;
         this.field = field;
         this.row = row;
+    }
+
+    public void store() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1521:xe", "root", "");
+
+            PreparedStatement ps = con.prepareStatement(
+                    "insert into filetable values(?,?)");
+
+            File f = new File("d:\\myfile.txt");
+            FileReader fr = new FileReader(f);
+
+            ps.setInt(1, 101);
+            ps.setCharacterStream(2, fr, (int) f.length());
+            int i = ps.executeUpdate();
+            System.out.println(i + " records affected");
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
