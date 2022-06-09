7aa792ef0030f0d3ecaafcba4a84549f707c65d6
Beatricia Teh <yuuesho@gmail.com>
2022-06-09 15:34:58 +0800

    fixed kebodohan

--- src/FarmerSimulation/Concurrent.java
+++ src/FarmerSimulation/Concurrent.java
@@ -21,33 +21,10 @@ public class Concurrent{
         }
         ExecutorService ex = Executors.newFixedThreadPool(threadNum);
         Farmer[] farmers = FarmerSimulator.generateFarmers(farmerNum);
-        //BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(10);
-        // ThreadPoolExecutor ex = new ThreadPoolExecutor(5,10, 5000, TimeUnit.MILLISECONDS, queue);
-        // ex.setRejectedExecutionHandler(new RejectedExecutionHandler() {
-        //      @Override
-        //     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-        //         System.out.println("Activity by thread "+Thread.currentThread().getName()+" rejected");
-                
-        //     };
-        // });
-        // BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>();
-        // CustomThreadPoolExecutor ex = new CustomThreadPoolExecutor(threadNum, farmerNum, 5, TimeUnit.SECONDS, queue);
-        
+       
         Timer cTimer = new Timer();
         cTimer.startTimer();
-        // while(true){
-        //     if(ex.getQueue().remainingCapacity()==0){
-        //         break;
-        //     }
-        // ex.prestartAllCoreThreads(); 
-        // for(int i=0;i<farmerNum;i++){
-        //     queue.offer(new FarmerThread(farmers[i].userId.toString()));
-        // }   
-        // ex.shutdown();
-        // }
-        // for(int i=0;i<10;i++){
-        //     ex.execute(new FarmerThread(String.valueOf(i)));
-        // }
+        
         for(int i=0;i<farmerNum;i++){
             Runnable farmer = new FarmerThread(farmers[i].userId.toString());
             ex.execute(farmer);
@@ -77,33 +54,9 @@ class FarmerThread implements Runnable{
     private void generateActivities(){
         try{
             FarmerSimulator.simulateActivity(this.farmerID);
-            Thread.sleep(2000);
+            Thread.sleep(10);
         }catch(InterruptedException e){
             e.printStackTrace();
         }
-    }
-}
-
-class CustomThreadPoolExecutor extends ThreadPoolExecutor {
-
-    public CustomThreadPoolExecutor(int corePoolSize, int maximumPoolSize,
-            long keepAliveTime, TimeUnit unit,
-            BlockingQueue<Runnable> workQueue) {
-        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-    }
-
-    @Override
-    protected void beforeExecute(Thread t, Runnable r) {
-        super.beforeExecute(t, r);
-        System.out.println("Perform beforeExecute() logic");
-    }
-
-    @Override
-    protected void afterExecute(Runnable r, Throwable t) {
-        super.afterExecute(r, t);
-        if (t != null) {
-            System.out.println("Perform exception handler logic");
-        }
-        System.out.println("Perform afterExecute() logic");
     }
 }
